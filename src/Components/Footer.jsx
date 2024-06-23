1ca4c520dfbd2e207b4293e2c298a3cd8dad9643
RiyaDeshwal <deshwalriya61@gmail.com>
2024-06-23 17:43:44 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Select from 'react-select';
+import { useTheme } from '../Context/ThemeContext';
+import { themeOptions } from '../Utils/theme';
+
+
+const Footer = () => {
+
+    const {theme, setTheme, defaultTheme} = useTheme();
+
+    const handleThemeChange = (e)=>{
+        console.log(e.value);
+        setTheme(e.value);
+        localStorage.setItem('theme', JSON.stringify(e.value));
+    }
+
+
+  return (
+    <div className='footer'>
+
+      <div className="intructions">
+        <div className="hint">
+          press <kbd>TAB</kbd> to open commands
+        </div>
+      </div>
+
+      <div className="actual-footer">
+       
+
+        <div className="themes">  
+          <Select
+                options={themeOptions}
+                onChange={handleThemeChange}
+                menuPlacement='top'
+                defaultValue={{value:defaultTheme, label: defaultTheme.label}}
+                styles={{
+                  control:  styles => ({...styles, backgroundColor: theme.background}),
+                  menu: styles => ({...styles, backgroundColor: theme.background}),
+                  option: (styles, {isFocused}) => {
+                      return {
+                          ...styles,
+                          backgroundColor: (isFocused)? theme.background : theme.textColor,
+                          color: (isFocused)? theme.textColor : theme.typeBoxText,
+                          cursor: 'pointer'
+                      }
+                  },
+                  singleValue: styles => ({...styles, color: theme.title}),
+              }}
+            />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Utils/theme.js
@@ -0,0 +1,48 @@
+
+
+const barbie = {
+    label: 'Barbie',
+    background: '#66004d',
+    title: '#f7eded',
+    typeBoxText: '#e8c9e0'
+}
+
+const  dark = {
+    label: 'Dark ',
+    background: '#091F2C',
+    title: '#A286B8',
+    typeBoxText: '#91E4D1',
+}
+
+const coco= {
+    label:"Coco",
+    background: "#584141",
+    title: "#ded3d3",
+    typeBoxText: "#9d7b7b",
+    stats: "#ded3d3"
+}
+
+const funk = {
+    label: " Funk",
+    background: "#2E1A47",
+    title: "#fff",
+    typeBoxText: "#C18FFF",
+    stats: "#fff"
+}
+
+const lilac = {
+    label: "Aether",
+    background: "#101820",
+    title: "#EEDAEA",
+    typeBoxText: "#CF6BDD",
+    stats: "#EEDAEA"
+}
+
+export const themeOptions = [
+    {value: barbie, label: 'Barbie'},
+    {value: dark, label: 'Dark '},
+    {value: coco, label: "Coco"},
+    {value: funk, label: " Funk"},
+    {value: lilac, label: "Lilac"}
+];
+
